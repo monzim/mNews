75ab885d6475ff70ba507b15e7c9ab7afb9af97e
MONZIM <monzim.code@gmail.com>
2020-12-04 00:05:53 +0600

    api data fetch complete

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:m_news/helper/data.dart';
 import 'package:m_news/models/category_model.dart';
+import 'package:m_news/models/article_model.dart';
+import 'package:m_news/helper/news.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -9,11 +11,26 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List<CategoryModel> categories = new List<CategoryModel>();
+  List<ArticleModel> articles = new List<ArticleModel>();
+
+  bool _loading = true;
+
   @override
   void initState() {
     // TODO
     super.initState();
     categories = getCategories();
+    getNews();
+  }
+
+  getNews() async {
+    News newsClass = News();
+    await newsClass.getNews();
+    articles = newsClass.news;
+
+    setState(() {
+      _loading = false;
+    });
   }
 
   @override
@@ -33,26 +50,51 @@ class _HomeState extends State<Home> {
         ),
         elevation: 0.0,
       ),
-      body: Container(
-        child: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 16),
-              height: 70,
-              child: ListView.builder(
-                  itemCount: categories.length,
-                  shrinkWrap: true,
-                  scrollDirection: Axis.horizontal,
-                  itemBuilder: (context, index) {
-                    return CategoryTile(
-                      imageUrl: categories[index].imageUrl,
-                      categoryName: categories[index].categoryName,
-                    );
-                  }),
+      body: _loading
+          ? Center(
+              child: Container(
+                child: CircularProgressIndicator(),
+              ),
             )
-          ],
-        ),
-      ),
+          : SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  children: [
+                    // catagories
+                    Container(
+                      padding: EdgeInsets.symmetric(horizontal: 16),
+                      height: 70,
+                      child: ListView.builder(
+                          itemCount: categories.length,
+                          shrinkWrap: true,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index) {
+                            return CategoryTile(
+                              imageUrl: categories[index].imageUrl,
+                              categoryName: categories[index].categoryName,
+                            );
+                          }),
+                    ),
+
+                    ///Blogs
+                    Container(
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: articles.length,
+                        scrollDirection: Axis.vertical,
+                        itemBuilder: (context, index) {
+                          return BlogTile(
+                              imageUrl: articles[index].urlToImage,
+                              title: articles[index].title,
+                              desc: articles[index].description);
+                        },
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
     );
   }
 }
@@ -108,8 +150,24 @@ class CategoryTile extends StatelessWidget {
 }
 
 class BlogTile extends StatelessWidget {
+  final String imageUrl, title, desc;
+
+  BlogTile({
+    @required this.imageUrl,
+    @required this.title,
+    @required this.desc,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Column(
+        children: [
+          Image.network(imageUrl),
+          Text(title),
+          Text(desc),
+        ],
+      ),
+    );
   }
 }
